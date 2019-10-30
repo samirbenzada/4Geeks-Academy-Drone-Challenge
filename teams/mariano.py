71ca717169800f9d9a4e5dcdb683c68d036f299c
Mariano Asselborn <marianoasselborn@gmail.com>
2019-10-30 19:30:30 -0400

    Let's see what happens

---
+++ teams/mariano.py
@@ -0,0 +1,93 @@
+"""
+4Geeks Hackathon
+"""
+import os
+from pyparrot.Minidrone import Mambo
+
+# CHANGE THIS?
+mamboAddr = os.environ.get('DRONE_ADDRESS', "D0:3A:F7:D9:E6:20")
+
+# Is it gonna be WIFI? ask.
+mambo = Mambo(mamboAddr, use_wifi=False)
+
+print("trying to connect")
+success = mambo.connect(num_retries=3)
+print("connected: %s" % success)
+
+if not success:
+	exit(1)
+
+
+print("sleeping")
+mambo.smart_sleep(2)
+mambo.ask_for_state_update()
+mambo.smart_sleep(2)
+
+print("taking off!")
+mambo.safe_takeoff(10)
+mambo.smart_sleep(4)
+
+
+print("dancing")
+mambo.turn_degrees(90)
+mambo.smart_sleep(1)
+mambo.turn_degrees(-90)
+mambo.smart_sleep(1)
+mambo.turn_degrees(-90)
+mambo.smart_sleep(1)
+mambo.turn_degrees(90)
+
+mambo.fly_direct(roll=0, pitch=10, yaw=0, vertical_movement=0, duration=1)
+mambo.fly_direct(roll=0, pitch=-10, yaw=0, vertical_movement=0, duration=1)
+mambo.turn_degrees(90)
+mambo.fly_direct(roll=0, pitch=10, yaw=0, vertical_movement=0, duration=1)
+mambo.fly_direct(roll=0, pitch=-10, yaw=0, vertical_movement=0, duration=1)
+mambo.turn_degrees(-180)
+mambo.fly_direct(roll=0, pitch=10, yaw=0, vertical_movement=0, duration=1)
+mambo.fly_direct(roll=0, pitch=-10, yaw=0, vertical_movement=0, duration=1)
+mambo.turn_degrees(90)
+
+mambo.smart_sleep(1)
+
+mambo.fly_direct(roll=25, pitch=0, yaw=50, vertical_movement=0, duration=3)
+mambo.fly_direct(roll=-25, pitch=0, yaw=-50, vertical_movement=0, duration=3)
+mambo.fly_direct(roll=25, pitch=0, yaw=50, vertical_movement=0, duration=3)
+mambo.fly_direct(roll=-25, pitch=0, yaw=-50, vertical_movement=0, duration=3)
+
+mambo.smart_sleep(1)
+
+mambo.flip(direction="back")
+mambo.smart_sleep(2)
+mambo.flip(direction="front")
+mambo.smart_sleep(2)
+
+mambo.fly_direct(roll=0, pitch=10, yaw=0, vertical_movement=0, duration=1)
+mambo.fly_direct(roll=0, pitch=-10, yaw=0, vertical_movement=0, duration=1)
+mambo.turn_degrees(90)
+mambo.fly_direct(roll=0, pitch=10, yaw=0, vertical_movement=0, duration=1)
+mambo.fly_direct(roll=0, pitch=-10, yaw=0, vertical_movement=0, duration=1)
+mambo.turn_degrees(-180)
+mambo.fly_direct(roll=0, pitch=10, yaw=0, vertical_movement=0, duration=1)
+mambo.fly_direct(roll=0, pitch=-10, yaw=0, vertical_movement=0, duration=1)
+mambo.turn_degrees(90)
+
+mambo.flip(direction="right")
+mambo.smart_sleep(2)
+mambo.flip(direction="left")
+mambo.smart_sleep(2)
+
+mambo.turn_degrees(90)
+mambo.smart_sleep(1)
+mambo.turn_degrees(-90)
+mambo.smart_sleep(1)
+mambo.turn_degrees(-90)
+mambo.smart_sleep(1)
+mambo.turn_degrees(90)
+
+mambo.safe_land(5)
+mambo.smart_sleep(5)
+
+mambo.disconnect()
+
+# End program
+exit(0)
